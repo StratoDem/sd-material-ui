6b231ee1dfc73e26da2303101551b213ee8312a5
michael <michael@stratodem.com>
2017-12-02 11:54:39 -0500

    Update Python version to 1.0.0

--- src/components/BottomNavigation.react.js
+++ src/components/BottomNavigation.react.js
@@ -6,17 +6,21 @@ import PropTypes from 'prop-types';
 import {
   BottomNavigationItem,
   BottomNavigation as MUIBottomNavigation } from 'material-ui/BottomNavigation';
-
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 type Props = {
   navItems: Array<{
     label: string,
     icon: Element | string,
     value: string | number,
+    url?: string,
   }>,
 
   setProps?: (props: {selectedIndex: number}) => void,
   selectedIndex?: number,
+  selectedStyle?: Object,
 };
 type State = {
   selectedIndex: number,
@@ -33,10 +37,15 @@ const propTypes = {
     label: PropTypes.string,
     icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    /** URL to jump to when this option is selected */
+    url: PropTypes.string,
   })).isRequired,
 
   /** Initial selected index for the BottomNavigation */
   selectedIndex: PropTypes.number,
+
+  /** Style to apply to the selected icon */
+  selectedStyle: PropTypes.objectOf(PropTypes.any),
 
   /**
    * Dash-assigned callback that should be called whenever any of the
@@ -55,6 +64,7 @@ export default class BottomNavigation extends React.Component<Props, State> {
   static defaultProps = {
     selectedIndex: 0,
     setProps: () => {},
+    selectedStyle: {},
   };
 
   constructor(props: Props) {
@@ -71,26 +81,38 @@ export default class BottomNavigation extends React.Component<Props, State> {
   }
 
   render() {
-    const { id, navItems } = this.props;
+    const { id, navItems, selectedStyle } = this.props;
 
     return (
       <div id={id}>
-        <MUIBottomNavigation selectedIndex={this.state.selectedIndex}>
-          {
-            navItems.map((navItem, selectedIndex) => (
-              <BottomNavigationItem
-                key={navItem.label}
-                label={navItem.label}
-                icon={navItem.icon}
-                onClick={() => {
-                  this.setState({selectedIndex});
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <MUIBottomNavigation selectedIndex={this.state.selectedIndex}>
+            {
+              navItems.map((navItem, selectedIndex) => (
+                <BottomNavigationItem
+                  key={navItem.label}
+                  label={navItem.label}
+                  icon={
+                    typeof navItem.icon === 'string'
+                      ? (
+                        <span
+                          style={selectedIndex === this.state.selectedIndex ? selectedStyle : {}}
+                        >
+                          {navItem.icon}
+                        </span>)
+                      : navItem.icon}
+                  onClick={() => {
+                    this.setState({selectedIndex});
 
-                  if (typeof this.props.setProps === 'function')
-                    this.props.setProps({selectedIndex});
-                }}
-              />))
-          }
-        </MUIBottomNavigation>
+                    if (typeof navItem.url === 'string')
+                      window.location.hash = navItem.url;
+                    if (typeof this.props.setProps === 'function')
+                      this.props.setProps({selectedIndex});
+                  }}
+                />))
+            }
+          </MUIBottomNavigation>
+        </MuiThemeProvider>
       </div>);
   }
 }
--- usage.py
+++ usage.py
@@ -7,19 +7,24 @@ app = dash.Dash('')
 app.scripts.config.serve_locally = True
 
 app.layout = html.Div([
-    sd_material_ui.ExampleComponent(
+    sd_material_ui.BottomNavigation(
         id='input',
-        value='my-value',
-        label='my-label'
-    ),
+        selectedIndex=0,
+        selectedStyle={'color': 'blue'},
+        navItems=[
+            dict(label='test 1', value='val1', icon='1'),
+            dict(label='test 2', value='val2', icon='2'),
+        ]),
     html.Div(id='output')
 ])
 
+
 @app.callback(
 	dash.dependencies.Output('output', 'children'),
-	[dash.dependencies.Input('input', 'value')])
+	[dash.dependencies.Input('input', 'selectedIndex')])
 def display_output(value):
     return 'You have entered {}'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
