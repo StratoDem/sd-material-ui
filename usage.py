1b474e7ca05701ccd7088a11a94fd0fde9eb1c48
Eric <eric@stratodem.com>
2017-12-26 16:50:11 -0500

    Add test for modal window

--- usage.py
+++ usage.py
@@ -17,13 +17,24 @@ app.layout = html.Div([
         ]),
     html.Div(id='output'),
     sd_material_ui.SDDialog(
-        html.Div('pathname'),
+        html.Div(children=[
+            html.P('pathname'),
+            html.P(id='closer', children='Close window'),
+        ]),
         id='dialog-id',
-        modal=False,
+        modal=True,
         open=False),
     html.Div(id='show-dialog', children=[
-        html.P('Share the page')
-    ])
+        html.P('Share the page (modal)')
+    ]),
+    sd_material_ui.SDDialog(
+        html.Div('pathname b'),
+        id='dialog-id-b',
+        modal=False,
+        open=False),
+    html.Div(id='show-dialog-b', children=[
+        html.P('Share the page b')
+    ]),
 ])
 
 
@@ -36,7 +47,23 @@ def display_output(value):
 
 @app.callback(
     dash.dependencies.Output('dialog-id', 'open'),
-    [dash.dependencies.Input('show-dialog', 'n_clicks')])
+    [dash.dependencies.Input('show-dialog', 'n_clicks'),
+     dash.dependencies.Input('closer', 'n_clicks')],
+    [dash.dependencies.State('dialog-id', 'open')])
+def show_dialog(open_button: int, close_button: int, open_state: bool):
+    if open_button:
+        if not open_state:
+            return True
+    elif close_button:
+        if open_state:
+            return False
+    else:
+        return False
+
+
+@app.callback(
+    dash.dependencies.Output('dialog-id-b', 'open'),
+    [dash.dependencies.Input('show-dialog-b', 'n_clicks')])
 def show_dialog(n_clicks: int):
     if n_clicks > 0:
         return True
