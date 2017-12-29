8cd46ed4696ebc52d5eae61c13cc456d4fca9423
Eric <eric@stratodem.com>
2017-12-29 09:43:38 -0500

    Add test (not passing)

--- src/components/__tests__/SDCheckbox.test.js
+++ src/components/__tests__/SDCheckbox.test.js
@@ -20,4 +20,15 @@ describe('SDCheckbox', () => {
     component.find('Checkbox').simulate('click');
     expect(clickCheck.mock.calls.length).toEqual(1);
   });
+
+  it('updates based on clicks', () => {
+    const component = shallow(
+      <SDCheckbox id='test-id' />);
+
+    expect(component.state().checked).toEqual(false);
+    expect(component.props().checked).toEqual(undefined);
+    component.simulate('click');
+    expect(component.state().checked).toEqual(true);
+    expect(component.props().checked).toEqual(true);
+  });
 });
