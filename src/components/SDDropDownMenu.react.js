8a7df0c6d17fe9d7137fdb88eb4be5c1ac7297cc
Eric <eric@stratodem.com>
2018-01-10 09:30:19 -0500

    Begin adding dropdown

---
+++ src/components/SDDropDownMenu.react.js
@@ -0,0 +1,199 @@
+// @flow
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import DropDownMenu from 'material-ui/DropDownMenu';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+type Props = {
+  anchorOrigin?: object, // ???
+  animated?: boolean,
+  animation?: () => void,
+  autoWidth?: boolean,
+  children?: Node,
+  className?: string,
+  disabled?: boolean,
+  fireEvent?: () => void,
+  iconButton?: Node,
+  iconStyle?: object,
+  id: string,
+  labelStyle?: object,
+  listStyle?: object,
+  maxHeight?: number,
+  menuItemStyle?: object,
+  menuStyle?: object,
+  multiple?: boolean,
+  openImmediately?: boolean,
+  selectedMenuItemStyle?: object,
+  setProps?: () => void,
+  style?: object,
+  targetOrigin?: object, // ???
+  underlineStyle?: object,
+  value?: any,
+};
+
+const propTypes = {
+  /**
+   * This is the point on the anchor that the popover's targetOrigin will attach to.
+   * Options: vertical: [top, center, bottom] horizontal: [left, middle, right].
+   */
+  anchorOrigin: PropTypes.objectOf(PropTypes.any), // ???
+
+  /**
+   * If true, the popover will apply transitions when it gets added to the DOM.
+   */
+  animated: PropTypes.bool,
+
+  /**
+   * Override the default animation component used.
+   */
+  animation: PropTypes.func,
+
+  /**
+   * The width will automatically be set according to the items inside the menu. To control
+   * this width in css instead, set this prop to false.
+   */
+  autoWidth: PropTypes.bool,
+
+  /**
+   * The MenuItems to populate the Menu with. If the MenuItems have the prop label that value will
+   * be used to render the representation of that item within the field.
+   */
+  children: PropTypes.node,
+
+  /**
+   * The css class name of the root element.
+   */
+  className: PropTypes.string,
+
+  /**
+   * Disables the menu.
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * A callback for firing events to dash.
+   */
+  fireEvent: PropTypes.func,
+
+  /**
+   * Overrides default SvgIcon dropdown arrow component.
+   */
+  iconButton: PropTypes.node,
+
+  /**
+   * Overrides the styles of icon element.
+   */
+  iconStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * The element's ID.
+   */
+  id: PropTypes.string.isRequired,
+
+  /**
+   * Overrides the styles of label when the DropDownMenu is inactive.
+   */
+  labelStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * The style object to use to override underlying list style.
+   */
+  listStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * The maximum height of the Menu when it is displayed.
+   */
+  maxHeight: PropTypes.number,
+
+  /**
+   * Override the inline-styles of menu items.
+   */
+  menuItemStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * Overrides the styles of Menu when the DropDownMenu is displayed.
+   */
+  menuStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * If true, value must be an array and the menu will support multiple selections.
+   */
+  multiple: PropTypes.bool,
+
+  /**
+   * Set to true to have the DropDownMenu automatically open on mount.
+   */
+  openImmediately: PropTypes.bool,
+
+  /**
+   * Override the inline-styles of selected menu items.
+   */
+  selectedMenuItemStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * Dash callback to update props on the server.
+   */
+  setProps: PropTypes.func,
+
+  /**
+   * Override the inline-styles of the root element.
+   */
+  style: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * This is the point on the popover which will attach to the anchor's origin.
+   * Options: vertical: [top, center, bottom] horizontal: [left, middle, right].
+   */
+  targetOrigin: PropTypes.objectOf(PropTypes.any), // ???
+
+  /**
+   * Overrides the inline-styles of the underline.
+   */
+  underlineStyle: PropTypes.objectOf(PropTypes.any),
+
+  /**
+   * If multiple is true, an array of the values of the selected menu items. Otherwise, the value
+   * of the selected menu item. If provided, the menu will be a controlled component.
+   */
+  value: PropTypes.any,
+};
+
+type State = {
+  open: boolean,
+};
+
+const defaultProps = {
+  anchorOrigin: { vertical: 'top', horizontal: 'left'},
+  animated: true,
+  animation: () => {},
+  autoWidth: true,
+  children: null,
+  className: '',
+  disabled: false,
+  fireEvent: () => {},
+  iconButton: Node,
+  iconStyle: {},
+  labelStyle: {},
+  listStyle: {},
+  maxHeight: 500,
+  menuItemStyle: {},
+  menuStyle: {},
+  multiple: false,
+  openImmediately: false,
+  selectedMenuItemStyle: {},
+  setProps: () => {},
+  style: {},
+  targetOrigin: {}, // ???
+  underlineStyle: {},
+  value: null,
+};
+
+export default class SDDropDownMenu extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+}
