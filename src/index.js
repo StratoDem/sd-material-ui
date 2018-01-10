37876a55b9ee815a9627bbd47b311b4b3ba516f2
Eric <eric@stratodem.com>
2018-01-10 09:40:27 -0500

    Add dropdown to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import BottomNavigation from './components/BottomNavigation.react';
 import SDCheckbox from './components/SDCheckbox.react';
 import SDDialog from './components/SDDialog.react';
 import SDDrawer from './components/SDDrawer.react';
+import SDDropDownMenu from './components/SDDropDownMenu.react';
 import SDFlatButton from './components/SDFlatButton.react';
 import SDRaisedButton from './components/SDRaisedButton.react';
 
@@ -10,6 +11,7 @@ export {
   SDCheckbox,
   SDDialog,
   SDDrawer,
+  SDDropDownMenu,
   SDFlatButton,
   SDRaisedButton,
 };
