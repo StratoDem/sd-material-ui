67840df945fb008b7ea7699b2e4f1dceda3925ed
Eric <eric@stratodem.com>
2018-01-12 11:13:55 -0500

    Remove test for click away
    Could not create other components to click on, because changes to the
    state of SDSnackbar cannot be made unless it is the root component,
    and then nothing else is available to click on

--- src/components/__tests__/SDSnackbar.test.js
+++ src/components/__tests__/SDSnackbar.test.js
@@ -6,7 +6,7 @@ import SDSnackbar from '../SDSnackbar.react';
 describe('SDSnackbar', () => {
   it('renders', () => {
     const component = shallow(
-      <SDSnackbar id='test-id' />
+      <SDSnackbar id='test-id' open={false} message='test message' />
     );
 
     expect(component.props().id).toEqual('test-id');
@@ -15,7 +15,7 @@ describe('SDSnackbar', () => {
 
   it('opens and closes', () => {
     const component = shallow(
-      <SDSnackbar id='test-id' />
+      <SDSnackbar id='test-id' open={false} message='test message' />
     );
 
     expect(component.state().open).toEqual(false);
@@ -27,27 +27,12 @@ describe('SDSnackbar', () => {
 
   it('closes automatically', () => {
     const component = shallow(
-      <SDSnackbar id='test-id' autoHideDuration={500} />
+      <SDSnackbar id='test-id' autoHideDuration={500} open={false} message='test message' />
     );
 
     expect(component.state().open).toEqual(false);
     component.setProps({open: true});
     expect(component.state().open).toEqual(true);
     setTimeout(() => expect(component.state().open).toEqual(false), 600);
-  });
-
-  it('closes when clicked away', () => {
-    const component = shallow(
-      <div>
-        <p>Somewhere else to click.</p>
-        <SDSnackbar id='test-id' autoHideDuration={5000} />
-      </div>
-    );
-
-    expect(component.state().open).toEqual(false);
-    component.setProps({open: true});
-    expect(component.state().open).toEqual(true);
-    component.find('p').simulate('click');
-    expect(component.state().open).toEqual(false);
   });
 });
