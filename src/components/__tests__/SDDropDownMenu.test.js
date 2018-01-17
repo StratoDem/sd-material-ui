bb305032b04847a513255e3b8c291025d500a7d5
Eric <eric@stratodem.com>
2018-01-17 12:14:33 -0500

    Update tests

--- src/components/__tests__/SDDropDownMenu.test.js
+++ src/components/__tests__/SDDropDownMenu.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import MenuItem from '../../../node_modules/material-ui/MenuItem';
 import SDDropDownMenu from '../SDDropDownMenu.react';
-import SDMenuItem from '../SDMenuItem.react';
 
 describe('SDDropDownMenu', () => {
   it('renders', () => {
@@ -16,28 +16,13 @@ describe('SDDropDownMenu', () => {
   it('updates value based on selections', () => {
     const component = mount(
       <SDDropDownMenu id='test-id' value={1}>
-        <SDMenuItem id='test-id2' value={1} primaryText='Test text' />
-        <SDMenuItem id='test-id3' value={2} primaryText='Test text' />
+        <MenuItem id='test-id2' value={1} primaryText='Test text' />
+        <MenuItem id='test-id3' value={2} primaryText='Test text' />
       </SDDropDownMenu>
     );
 
     expect(component.state('value')).toEqual(1);
     component.find('DropDownMenu').props().onChange({}, 1, 2);
     expect(component.state('value')).toEqual(2);
-  });
-
-  it('handles multiple selections', () => {
-    const component = mount(
-      <SDDropDownMenu id='test-id' multiple={true} value={[1]}>
-        <SDMenuItem id='test-id2' value={1} primaryText='Test text' />
-        <SDMenuItem id='test-id3' value={2} primaryText='Test text' />
-      </SDDropDownMenu>
-    );
-
-    expect(component.state('value')).toEqual([1]);
-    component.find('DropDownMenu').props().onChange({}, 1, [1, 2]);
-    expect(component.state('value')).toEqual([1, 2]);
-    component.find('DropDownMenu').props().onChange({}, 1, [2]);
-    expect(component.state('value')).toEqual([2]);
   });
 });
