86917df129117a2581f1c5ae0457674104a8d58c
Eric <eric@stratodem.com>
2018-01-31 11:43:26 -0500

    Correct spelling error

--- src/components/List/ListItem.react.js
+++ src/components/List/ListItem.react.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import PropTyes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import { ListItem as MuiListItem } from 'material-ui/List';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
@@ -170,12 +170,12 @@ type propTypes = {
    * Note that an indicator will not be created if a rightIcon or rightIconButton has been
    * provided to the element.
    */
-  autoGenerateNestedIndicator: PropTyes.bool,
+  autoGenerateNestedIndicator: PropTypes.bool,
 
   /**
    * Children passed into the ListItem.
    */
-  children: PropTyes.node,
+  children: PropTypes.node,
 
   /**
    * The element to use as the container for the ListItem. Either a string to use a DOM element
@@ -183,49 +183,49 @@ type propTypes = {
    * If a ReactElement is given, ensure that it passes all of its given props through to the
    * underlying DOM element and renders its children prop for proper integration.
    */
-  containerElement: PropTyes.oneOfType([PropTyes.string, PropTyes.element]),
+  containerElement: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 
   /**
    * If true, the element will not be able to be focused by the keyboard.
    */
-  disabledKeyboardFocus: PropTyes.bool,
+  disabledKeyboardFocus: PropTypes.bool,
 
   /**
    * If true, the element will not be clickable and will not display hover effects.
    * This is automatically disabled if either leftCheckbox or rightToggle is set.
    */
-  disabled: PropTyes.bool,
+  disabled: PropTypes.bool,
 
   /**
    * Dash-assigned callback that gets fired when the input changes.
    */
-  fireEvent: PropTyes.func,
+  fireEvent: PropTypes.func,
 
   /**
    * Override the hover background color.
    */
-  hoverColor: PropTyes.string,
+  hoverColor: PropTypes.string,
 
   /**
    * The element's ID.
    */
-  id: PropTyes.string,
+  id: PropTypes.string,
 
   /**
    * If true, the nested ListItems are initially displayed.
    */
-  initiallyOpen: PropTyes.bool,
+  initiallyOpen: PropTypes.bool,
 
   /**
    * Override the inline-styles of the inner div element.
    */
-  innerDivStyle: PropTyes.object,
+  innerDivStyle: PropTypes.object,
 
   /**
    * If true, the children will be indented by 72px. This is useful if there is no left
    * avatar or left icon.
    */
-  insetChildren: PropTyes.bool,
+  insetChildren: PropTypes.bool,
 
   /**
    * Use to control if the list item should render as keyboard focused. If undefined (default),
@@ -233,91 +233,91 @@ type propTypes = {
    * Note that this will not change the actual focus - and should only be used when you want to
    * simulate keyboard focus (eg. in a rich text input autocomplete).
    */
-  isKeyboardFocused: PropTyes.bool,
+  isKeyboardFocused: PropTypes.bool,
 
   /**
    * This is the Avatar element to be displayed on the left side.
    */
-  leftAvatar: PropTyes.element,
+  leftAvatar: PropTypes.element,
 
   /**
    * This is the Checkbox element to be displayed on the left side.
    */
-  leftCheckbox: PropTyes.element,
+  leftCheckbox: PropTypes.element,
 
   /**
    * This is the SvgIcon or FontIcon to be displayed on the left side.
    */
-  leftIcon: PropTyes.element,
+  leftIcon: PropTypes.element,
 
   /**
    * An array of ListItems to nest underneath the current ListItem.
    */
-  nestedItems: PropTyes.array,
+  nestedItems: PropTypes.array,
 
   /**
    * Override the inline-styles of the nested items' NestedList.
    */
-  nestedListStyle: PropTyes.object,
+  nestedListStyle: PropTypes.object,
 
   /**
    * Control toggle state of nested list.
    */
-  open: PropTyes.bool,
+  open: PropTypes.bool,
 
   /**
    * This is the block element that contains the primary text. If a string is passed in,
    * a div tag will be rendered.
    */
-  primaryText: PropTyes.node,
+  primaryText: PropTypes.node,
 
   /**
    * If true, clicking or tapping the primary text of the ListItem toggles the nested list.
    */
-  primaryTogglesNestedList: PropTyes.bool,
+  primaryTogglesNestedList: PropTypes.bool,
 
   /**
    * This is the Avatar element to be displayed on the right side.
    */
-  rightAvatar: PropTyes.element,
+  rightAvatar: PropTypes.element,
 
   /**
    * This is the SvgIcon or FontIcon to be displayed on the right side.
    */
-  rightIcon: PropTyes.element,
+  rightIcon: PropTypes.element,
 
   /**
    * This is the IconButton to be displayed on the right side. Hovering over this button will
    * remove the ListItem hover. Also, clicking on this button will not trigger a ripple on the
    * ListItem; the event will be stopped and prevented from bubbling up to cause a ListItem click.
    */
-  rightIconButton: PropTyes.element,
+  rightIconButton: PropTypes.element,
 
   /**
    * This is the Toggle element to display on the right side.
    */
-  rightToggle: PropTyes.element,
+  rightToggle: PropTypes.element,
 
   /**
    * This is the block element that contains the secondary text. If a string is passed in,
    * a div tag will be rendered.
    */
-  secondaryText: PropTyes.node,
+  secondaryText: PropTypes.node,
 
   /**
    * Can be 1 or 2. This is the number of secondary text lines before ellipsis will show.
    */
-  secondaryTextLines: PropTyes.oneOf([1, 2]),
+  secondaryTextLines: PropTypes.oneOf([1, 2]),
 
   /**
    * Dash callback to update props on the server.
    */
-  setProps: PropTyes.func,
+  setProps: PropTypes.func,
 
   /**
    * Override the inline-styles of the root element.
    */
-  style: PropTyes.object,
+  style: PropTypes.object,
 };
 
 type State = {
