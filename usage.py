fdfcf5089899652e343e84906dc0ca56fe388ac2
Eric <eric@stratodem.com>
2018-08-29 14:33:57 -0400

    Reduce line height of text

--- usage.py
+++ usage.py
@@ -198,7 +198,17 @@ app.layout = html.Div([
 
     # Test for SDToggle
     html.Div(children=[
-        sd_material_ui.Toggle(id='input9', label='Johnny?'),
+        sd_material_ui.Toggle(
+            id='input9',
+            label='Johnny?',
+            iconStyle=dict(height=16, lineHeight=16, width=36),
+            labelStyle=dict(fontWeight=300, fontSize=14, lineHeight=16),
+            thumbStyle=dict(height=16, width=16),
+            thumbSwitchedStyle=dict(height=16, width=16),
+            trackStyle=dict(height=9, width=36),
+            trackSwitchedStyle=dict(height=9, width=36),
+            style=dict(height=16, lineHeight=16),
+        ),
         html.Div(id='output9', children=[
             html.P('Flame off')
         ]),
@@ -303,63 +313,63 @@ app.layout = html.Div([
 
     spacer,
 
-    sd_material_ui.Questions(
-        id='questions-id',
-        questionSectionProps=[
-            dict(
-                headerTitle='Test title',
-                questionProps=[
-                    dict(
-                        questionText='How do I do something?',
-                        questionType='Tag my question',
-                        answerPrompt='Ask the question',
-                        value='this-is-a-new-value'),
-                ])
-        ],
-        value='',
-    ),
-
-    html.Div(id='question-output-id', children=''),
-    html.Div(id='question-output-id2', children=''),
+    # sd_material_ui.Questions(
+    #     id='questions-id',
+    #     questionSectionProps=[
+    #         dict(
+    #             headerTitle='Test title',
+    #             questionProps=[
+    #                 dict(
+    #                     questionText='How do I do something?',
+    #                     questionType='Tag my question',
+    #                     answerPrompt='Ask the question',
+    #                     value='this-is-a-new-value'),
+    #             ])
+    #     ],
+    #     value='',
+    # ),
+    #
+    # html.Div(id='question-output-id', children=''),
+    # html.Div(id='question-output-id2', children=''),
+    #
+    # spacer,
+    #
+    # sd_material_ui.QuestionsTabs(
+    #     id='questions-tabs-id',
+    #     questionSectionProps=[
+    #         dict(
+    #             headerTitle='Test title tab 1',
+    #             questionProps=[
+    #                 dict(
+    #                     questionText='How do I do something?',
+    #                     value='this-is-a-new-value'),
+    #             ]),
+    #         dict(
+    #             headerTitle='Test title tab 2',
+    #             questionProps=[
+    #                 dict(
+    #                     questionText='How do I do something else?',
+    #                     value='this-is-a-second-value'),
+    #             ]),
+    #     ],
+    #     value='',
+    # ),
+    #
+    # html.Div(id='question-output-id3', children=''),
+    # html.Div(id='question-output-id4', children=''),
 
     spacer,
 
-    sd_material_ui.QuestionsTabs(
-        id='questions-tabs-id',
-        questionSectionProps=[
-            dict(
-                headerTitle='Test title tab 1',
-                questionProps=[
-                    dict(
-                        questionText='How do I do something?',
-                        value='this-is-a-new-value'),
-                ]),
-            dict(
-                headerTitle='Test title tab 2',
-                questionProps=[
-                    dict(
-                        questionText='How do I do something else?',
-                        value='this-is-a-second-value'),
-                ]),
-        ],
-        value='',
-    ),
-
-    html.Div(id='question-output-id3', children=''),
-    html.Div(id='question-output-id4', children=''),
-
-    spacer,
-
-    sd_material_ui.Tabs(
-        children=[
-            html.Div('Tab 1'),
-            html.Div('Tab 2'),
-        ],
-        tabPropsArray=[
-            {'label': 'Tab 1 label'},
-            {'label': 'Tab 2 label'},
-        ]
-    ),
+    # sd_material_ui.Tabs(
+    #     children=[
+    #         html.Div('Tab 1'),
+    #         html.Div('Tab 2'),
+    #     ],
+    #     tabPropsArray=[
+    #         {'label': 'Tab 1 label'},
+    #         {'label': 'Tab 2 label'},
+    #     ]
+    # ),
 
     sd_material_ui.AutoComplete(
         id='input-autocomplete-search',
@@ -377,49 +387,49 @@ app.layout = html.Div([
 ])
 
 
-@app.callback(
-    dash.dependencies.Output('question-output-id', 'children'),
-    [
-        dash.dependencies.Input('questions-id', 'value'),
-        dash.dependencies.Input('questions-id', 'n_clicks'),
-     ],
-    [dash.dependencies.State('questions-id', 'n_clicks_previous')])
-def update_questions_output(value, n_clicks, n_clicks_previous):
-    time.sleep(5)
-    return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
+# @app.callback(
+#     dash.dependencies.Output('question-output-id', 'children'),
+#     [
+#         dash.dependencies.Input('questions-id', 'value'),
+#         dash.dependencies.Input('questions-id', 'n_clicks'),
+#      ],
+#     [dash.dependencies.State('questions-id', 'n_clicks_previous')])
+# def update_questions_output(value, n_clicks, n_clicks_previous):
+#     time.sleep(5)
+#     return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
+#
+#
+# @app.callback(
+#     dash.dependencies.Output('question-output-id2', 'children'),
+#     [
+#         dash.dependencies.Input('questions-id', 'value'),
+#         dash.dependencies.Input('questions-id', 'n_clicks'),
+#      ],
+#     [dash.dependencies.State('questions-id', 'n_clicks_previous')])
+# def update_questions_output(value, n_clicks, n_clicks_previous):
+#     return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
 
 
-@app.callback(
-    dash.dependencies.Output('question-output-id2', 'children'),
-    [
-        dash.dependencies.Input('questions-id', 'value'),
-        dash.dependencies.Input('questions-id', 'n_clicks'),
-     ],
-    [dash.dependencies.State('questions-id', 'n_clicks_previous')])
-def update_questions_output(value, n_clicks, n_clicks_previous):
-    return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
-
-
-@app.callback(
-    dash.dependencies.Output('question-output-id3', 'children'),
-    [
-        dash.dependencies.Input('questions-tabs-id', 'value'),
-        dash.dependencies.Input('questions-tabs-id', 'n_clicks'),
-     ],
-    [dash.dependencies.State('questions-tabs-id', 'n_clicks_previous')])
-def update_questions_output(value, n_clicks, n_clicks_previous):
-    return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
-
-
-@app.callback(
-    dash.dependencies.Output('question-output-id4', 'children'),
-    [
-        dash.dependencies.Input('questions-tabs-id', 'value'),
-        dash.dependencies.Input('questions-tabs-id', 'n_clicks'),
-     ],
-    [dash.dependencies.State('questions-tabs-id', 'n_clicks_previous')])
-def update_questions_output(value, n_clicks, n_clicks_previous):
-    return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
+# @app.callback(
+#     dash.dependencies.Output('question-output-id3', 'children'),
+#     [
+#         dash.dependencies.Input('questions-tabs-id', 'value'),
+#         dash.dependencies.Input('questions-tabs-id', 'n_clicks'),
+#      ],
+#     [dash.dependencies.State('questions-tabs-id', 'n_clicks_previous')])
+# def update_questions_output(value, n_clicks, n_clicks_previous):
+#     return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
+#
+#
+# @app.callback(
+#     dash.dependencies.Output('question-output-id4', 'children'),
+#     [
+#         dash.dependencies.Input('questions-tabs-id', 'value'),
+#         dash.dependencies.Input('questions-tabs-id', 'n_clicks'),
+#      ],
+#     [dash.dependencies.State('questions-tabs-id', 'n_clicks_previous')])
+# def update_questions_output(value, n_clicks, n_clicks_previous):
+#     return '{} {} {}'.format(value, n_clicks, n_clicks_previous)
 
 
 @app.callback(
