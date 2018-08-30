f1eb78a28f4ad18a7957c851ce7d8d078e2c22c6
Eric <eric@stratodem.com>
2018-08-30 08:33:27 -0400

    Add test for rendering children, remove tests for things that cannot be tested

--- src/components/__tests__/Popover.test.js
+++ src/components/__tests__/Popover.test.js
@@ -6,38 +6,17 @@ import Popover from '../Popover/Popover.react';
 describe('Popover', () => {
   it('renders', () => {
     const component = shallow(
-      <Popover/>
+      <Popover />
     );
-
     expect(component).toBe.ok;
   });
 
-  it('updates props correctly', () => {
+  it('renders children', () => {
     const component = shallow(
-      <Popover/>);
+      <Popover>
+        children=<div className='myDiv' />
+      </Popover>);
 
-    expect(component.state('open')).toEqual(false);
-    component.setProps({open: true});
-    expect(component.state('open')).toEqual(true);
-    component.setProps({open: false});
-    expect(component.state('open')).toEqual(false);
-  });
-
-  it('handles click events', () => {
-    const component = mount(
-      <Popover/>);
-
-    expect(component.state('open')).toEqual(false);
-    component.find(RaisedButton).props().onClick({}, true);
-    expect(component.state('open')).toEqual(true);
-  });
-
-  it('sets state even without setProps/fireEvent', () => {
-    const component = shallow(
-      <Popover setProps={null} fireEvent={null} />);
-
-    expect(component.state('open')).toEqual(false);
-    component.find(RaisedButton).props().onClick({}, true);
-    expect(component.state('open')).toEqual(true);
+    expect(component.contains(<div className="myDiv" />)).toEqual(true);
   });
 });
