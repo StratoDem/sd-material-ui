9eb6fe738ac6cc6a5b0fdd8f11dab2dea4b9234b
michael <michael@stratodem.com>
2019-07-10 10:54:15 -0400

    3.1.2

--- sd_material_ui/Dialog.py
+++ sd_material_ui/Dialog.py
@@ -22,14 +22,21 @@ already open or closed before attempting to open or close it. See comments for t
 IMPORTANT: When using this component in Dash, a listener must be set up (either as state or
 an input) for this component's props.open value in order to achieve the desired behavior.
 If such a listener is not in place, the non-modal version of this dialog will contaminate
-other callbacks in the browser"""
+other callbacks in the browser
+- autoScrollBodyContent (boolean; optional): If set to true, the body content of the Dialog will be scrollable.
+- actionsContainerClassName (string; optional): The className to add to the actions container's root element.
+- bodyClassName (string; optional): The className to add to the content's root element under the title.
+- contentClassName (string; optional): The className to add to the content container
+- overlayClassName (string; optional): The className to add to the Overlay component rendered behind the Dialog
+- paperClassName (string; optional): CSS class name of the Paper element
+- titleClassName (string; optional): The className to add to the title's root container element"""
     @_explicitize_args
-    def __init__(self, children=None, id=Component.REQUIRED, actions=Component.UNDEFINED, className=Component.UNDEFINED, modal=Component.UNDEFINED, open=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['children', 'id', 'actions', 'className', 'modal', 'open']
+    def __init__(self, children=None, id=Component.REQUIRED, actions=Component.UNDEFINED, className=Component.UNDEFINED, modal=Component.UNDEFINED, open=Component.UNDEFINED, autoScrollBodyContent=Component.UNDEFINED, actionsContainerClassName=Component.UNDEFINED, bodyClassName=Component.UNDEFINED, contentClassName=Component.UNDEFINED, overlayClassName=Component.UNDEFINED, paperClassName=Component.UNDEFINED, titleClassName=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'actions', 'className', 'modal', 'open', 'autoScrollBodyContent', 'actionsContainerClassName', 'bodyClassName', 'contentClassName', 'overlayClassName', 'paperClassName', 'titleClassName']
         self._type = 'Dialog'
         self._namespace = 'sd_material_ui'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['children', 'id', 'actions', 'className', 'modal', 'open']
+        self.available_properties = ['children', 'id', 'actions', 'className', 'modal', 'open', 'autoScrollBodyContent', 'actionsContainerClassName', 'bodyClassName', 'contentClassName', 'overlayClassName', 'paperClassName', 'titleClassName']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
