48412b26f87a1747ee6dc4d2ee1629011ef223bc
Danny Kaminsky <dwkaminsky@gmail.com>
2020-08-12 11:57:37 -0400

    Continued updating buttons

--- src/lib/components/FlatButton.react.js
+++ src/lib/components/FlatButton.react.js
@@ -5,20 +5,59 @@ import {Button as MUIButton} from '@material-ui/core/Button';
 import {IconButton as MUIIconButton} from '@material-ui/core/IconButton';
 
 type Props = {
-  /** Button ID */
-  id: string,
-  /** Children to render inside of the Button */
+  /** Button color when *no* hover event is triggered */
+  backgroundColor?: string,
+  /**
+   * This is what will be displayed inside the button.
+   * If a label is specified, the text within the label prop will
+   * be displayed. Otherwise, the component will expect children
+   * which will then be displayed. (In our example,
+   * we are nesting an `<input type="file" />` and a `span`
+   * that acts as our label to be displayed.) This only
+   * applies to flat and raised buttons.
+   */
   children?: Node,
   /** CSS class name of the root element */
   className?: string,
-  /** Is the Button raised? */
-  raised?: boolean,
-  /** Is the Button an IconButton? */
-  useIcon?: boolean,
-  /** Is the Button disabled? */
+  /** If true, the element's ripple effect will be disabled */
+  disableTouchRipple?: boolean,
+  /** Disable the button? */
   disabled?: boolean,
-  /** Dash callback to update props on the server */
-  setProps?: (props: { modal?: boolean, open?: boolean }) => void,
+  /** Dash callback to trigger an event handler */
+  fireEvent?: () => void,
+  /** If true, the button will take up the full width of its container */
+  fullWidth?: boolean,
+  /** Color of button when mouse hovers over */
+  hoverColor?: string,
+  /** The URL to link to when the button is clicked */
+  href?: string,
+  /** Use this property to display an icon */
+  icon?: Node,
+  /** Element ID */
+  id?: string,
+  /** Label for the button */
+  label: string,
+  /** Place label before or after the passed children */
+  labelPosition?: 'before' | 'after',
+  /** Override the inline styles of the button's label element */
+  labelStyle?: Object,
+  /** An integer that represents the number fo times that this element has been clicked */
+  n_clicks?: number,
+  /** An integer that represents the previous number of times this element has been clicked */
+  n_clicks_previous?: number,
+  /** If true, colors button according to primaryTextColor from the MuiTheme */
+  primary?: boolean,
+  /** Color for the ripple when the button is clicked */
+  rippleColor?: string,
+  /**
+   * If true, colors button according to secondaryTextColor from the theme.
+   * The primary prop has precendent if set to true.
+   */
+  secondary?: boolean,
+  /** Dash callback to update props */
+  setProps?: () => void,
+  /** Override the inline styles of the root element */
+  style?: Object,
 };
 
 type State = {
@@ -74,3 +113,157 @@ export default class Button extends Component<Props> {
 }
 
 Button.defaultProps = defaultProps;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+type Props = {
+  /** Button color when *no* hover event is triggered */
+  backgroundColor?: string,
+  /**
+   * This is what will be displayed inside the button.
+   * If a label is specified, the text within the label prop will
+   * be displayed. Otherwise, the component will expect children
+   * which will then be displayed. (In our example,
+   * we are nesting an `<input type="file" />` and a `span`
+   * that acts as our label to be displayed.) This only
+   * applies to flat and raised buttons.
+   */
+  children?: Node,
+  /** CSS class name of the root element */
+  className?: string,
+  /** If true, the element's ripple effect will be disabled */
+  disableTouchRipple?: boolean,
+  /** Disable the button? */
+  disabled?: boolean,
+  /** Dash callback to trigger an event handler */
+  fireEvent?: () => void,
+  /** If true, the button will take up the full width of its container */
+  fullWidth?: boolean,
+  /** Color of button when mouse hovers over */
+  hoverColor?: string,
+  /** The URL to link to when the button is clicked */
+  href?: string,
+  /** Use this property to display an icon */
+  icon?: Node,
+  /** Element ID */
+  id?: string,
+  /** Label for the button */
+  label: string,
+  /** Place label before or after the passed children */
+  labelPosition?: 'before' | 'after',
+  /** Override the inline styles of the button's label element */
+  labelStyle?: Object,
+  /** An integer that represents the number fo times that this element has been clicked */
+  n_clicks?: number,
+  /** An integer that represents the previous number of times this element has been clicked */
+  n_clicks_previous?: number,
+  /** If true, colors button according to primaryTextColor from the MuiTheme */
+  primary?: boolean,
+  /** Color for the ripple when the button is clicked */
+  rippleColor?: string,
+  /**
+   * If true, colors button according to secondaryTextColor from the theme.
+   * The primary prop has precendent if set to true.
+   */
+  secondary?: boolean,
+  /** Dash callback to update props */
+  setProps?: () => void,
+  /** Override the inline styles of the root element */
+  style?: Object,
+};
+
+const defaultProps = {
+  backgroundColor: '',
+  children: null,
+  className: '',
+  disableTouchRipple: false,
+  disabled: false,
+  fireEvent: () => {},
+  fullWidth: false,
+  hoverColor: '',
+  href: '',
+  icon: null,
+  labelPosition: 'after',
+  labelStyle: {},
+  n_clicks: 0,
+  n_clicks_previous: 0,
+  primary: false,
+  rippleColor: '',
+  secondary: false,
+  setProps: () => {},
+  style: {},
+};
+
+/** Material UI FlatButton component */
+export default class FlatButton extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.props.setProps) this.props.setProps({n_clicks: this.props.n_clicks + 1});
+    if (this.props.fireEvent) this.props.fireEvent({event: 'click'});
+    if (this.props.setProps) this.props.setProps({n_clicks_previous: this.props.n_clicks + 1});
+  }
+
+  render() {
+    const { backgroundColor, className, disableTouchRipple, disabled,
+      fullWidth, hoverColor, href, icon, id, label, labelPosition, labelStyle,
+      primary, rippleColor, secondary, style} = this.props;
+
+    if (this.props.fireEvent || this.props.setProps) {
+      return (
+        <div id={id}>
+          <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+            <MuiFlatButton
+              backgroundColor={backgroundColor}
+              className={className}
+              disableTouchRipple={disableTouchRipple}
+              disabled={disabled}
+              fullWidth={fullWidth}
+              hoverColor={hoverColor}
+              href={href}
+              icon={icon}
+              label={label}
+              labelPosition={labelPosition}
+              labelStyle={labelStyle}
+              onClick={this.handleClick}
+              primary={primary}
+              rippleColor={rippleColor}
+              secondary={secondary}
+              style={style}
+            >
+              {this.props.children}
+            </MuiFlatButton>
+          </MuiThemeProvider>
+        </div>);
+    }
+    return (
+      <div id={id}>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <MuiFlatButton
+            backgroundColor={backgroundColor}
+            className={className}
+            disableTouchRipple={disableTouchRipple}
+            disabled={disabled}
+            fullWidth={fullWidth}
+            hoverColor={hoverColor}
+            href={href}
+            icon={icon}
+            label={label}
+            labelPosition={labelPosition}
+            labelStyle={labelStyle}
+            primary={primary}
+            rippleColor={rippleColor}
+            secondary={secondary}
+            style={style}
+          >
+            {this.props.children}
+          </MuiFlatButton>
+        </MuiThemeProvider>
+      </div>);
+  }
+}
+
+FlatButton.defaultProps = defaultProps;
+
