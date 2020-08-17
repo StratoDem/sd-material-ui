27dae22343a7c11e69d28e54f6ad82e324a33118
Danny Kaminsky <dwkaminsky@gmail.com>
2020-08-17 13:09:36 -0400

    Updated package

--- sd_material_ui/_imports_.py
+++ sd_material_ui/_imports_.py
@@ -1,13 +1,17 @@
 from .Button import Button
+from .Card import Card
 from .Dialog import Dialog
 from .Drawer import Drawer
+from .Paper import Paper
 from .RadioButtonGroup import RadioButtonGroup
 from .Toggle import Toggle
 
 __all__ = [
     "Button",
+    "Card",
     "Dialog",
     "Drawer",
+    "Paper",
     "RadioButtonGroup",
     "Toggle"
 ]
