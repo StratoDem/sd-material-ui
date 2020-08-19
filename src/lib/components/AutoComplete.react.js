883622f6b2c8dae8d8fda7c4a99706e296ece989
Danny Kaminsky <dwkaminsky@gmail.com>
2020-08-19 12:52:46 -0400

    Began updating AutoComplete.react.js

---
+++ src/lib/components/AutoComplete.react.js
@@ -0,0 +1,272 @@
+// @flow
+
+import React, { Component } from 'react';
+
+import MuiAutoComplete from '@material-ui/lab/Autocomplete'
+import TextField from '@material-ui/core/TextField';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import _ from 'lodash';
+
+type Props = {
+  /** The classes to be applied to this component. This keys in this object must be valid CSS rule
+   * names, and the values must be strings for the classnames to be assigned to each rule name
+   * Valid rule names are:
+   *   root
+   *   fullWidth
+   *   focus
+   *   tag
+   *   tagSizeSmall
+   *   hasPopupIcon
+   *   hasClearIcon
+   *   inputRoot
+   *   input
+   *   inputFocused
+   *   endAdornment
+   *   clearIndicator
+   *   clearIndicatorDirty
+   *   popupIndicator
+   *   popupIndicatorOpen
+   *   popper
+   *   popperDisablePortal
+   *   paper
+   *   listbox
+   *   loading
+   *   noOptions
+   *   option
+   *   groupLabel
+   *   groupUl
+   * */
+  classes?: {
+    root?: string,
+    fullWidth?: string,
+    focus?: string,
+    tag?: string,
+    tagSizeSmall?: string,
+    hasPopupIcon?: string,
+    hasClearIcon?: string,
+    inputRoot?: string,
+    input?: string,
+    inputFocused?: string,
+    endAdornment?: string,
+    clearIndicator?: string,
+    clearIndicatorDirty?: string,
+    popupIndicator?: string,
+    popupIndicatorOpen?: string,
+    popper?: string,
+    popperDisablePortal?: string,
+    paper?: string,
+    listbox?: string,
+    loading?: string,
+    noOptions?: string,
+    option?: string,
+    groupLabel?: string,
+    groupUl?: string,
+  },
+  /** CSS class name of the root element */
+  className?: string,
+  /** Dash callback delay in ms - default is 500 ms */
+  dashCallbackDelay?: number,
+  /**
+   * Array of strings or nodes used to populate the list
+   * Alternatively, an Array of Objects with a structure like
+   * {label: 'My label to render', value: 'My value to ship on match'}
+   */
+  dataSource?: Array<any>,
+  /** Should the search text have to match exactly to update props server side? */
+  exactMatch?: boolean,
+  /** String name for filter to be applied to user input.
+   * will later be mapped to function
+   */
+  filter?: 'caseInsensitiveFilter' | 'caseSensitiveFilter' | 'defaultFilter' |
+    'fuzzyFilter' | 'levenshteinDistanceFilter' | 'noFilter',
+  /** Dash-assigned callback that gets fired when the input changes. */
+  fireEvent?: () => void,
+  /** The content to use for adding floating label element */
+  floatingLabelText?: Node,
+  /** If true, field receives the property width: 100% */
+  fullWidth?: boolean,
+  /** The hint content to display */
+  hintText?: Node,
+  /** Autocomplete ID */
+  id: string,
+  /** The max number of search results to be shown. By default it shows
+   * all the items which matches filter */
+  maxSearchResults?: number,
+  /** Delay for closing time of the menu */
+  menuCloseDelay?: number,
+  /** Props to be passed to menu */
+  menuProps?: Object,
+  /** Override style for menu */
+  menuStyle?: Object,
+  /** Auto complete menu is open if true */
+  open?: boolean,
+  /** If true, the list item is showed when a focus event triggers */
+  openOnFocus?: boolean,
+  /** Props to be passed to popover */
+  popoverProps?: Object,
+  /** Text being input to auto complete */
+  searchText?: string,
+  /** Value in the dataSource found by using searchText
+   * NOTE exactMatch must be true for this to work
+   */
+  searchValue?: any,
+  /** Dash callback to update props on the server. */
+  setProps?: () => void,
+  /** Override the inline-styles of the root element */
+  style?: Object,
+  /** If defined, the AutoComplete component hits this URL to search instead of string matching */
+  searchEndpointAPI?: string,
+  /** General JSON structure to send to the server */
+  searchJSONStructure?: Object,
+};
+
+type State = {
+  searchText: string,
+  dataSourceRender: Array<string>,
+}
+
+const defaultProps = {
+  dashCallbackDelay: 500,
+  dataSource: [],
+  exactMatch: false,
+  filter: "defaultFilter",
+  fireEvent: () => {},
+  hintText: null,
+  maxSearchResults: 5,
+  menuCloseDelay: 300,
+  open: false,
+  openOnFocus: false,
+  searchText: "",
+  setProps: () => {},
+  style: {},
+  searchValue: null,
+  searchEndpointAPI: undefined,
+  searchJSONStructure: {},
+};
+
+const mapFilterToFunc = {
+  caseInsensitiveFilter: MuiAutoComplete.caseInsensitiveFilter,
+  caseSensitiveFilter: MuiAutoComplete.caseSensitiveFilter,
+  defaultFilter: MuiAutoComplete.defaultFilter,
+  fuzzyFilter: MuiAutoComplete.fuzzyFilter,
+  levenshteinDistanceFilter: MuiAutoComplete.levenshteinDistanceFilter,
+  noFilter: MuiAutoComplete.noFilter,
+};
+
+/** Material UI AutoComplete component */
+export default class AutoComplete extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      searchText: this.props.searchText,
+      dataSourceRender: this.props.exactMatch
+        ? this.props.dataSource.map(d => d.label)
+        : this.props.dataSource,
+    };
+    /** _.debounce used to provide delay in callback to avoid firing callback every
+     * time user input changes - waits this.props.dashCallbackDelay ms to fire callback */
+    this.updateTextProps = _.debounce(this._updateTextProps, this.props.dashCallbackDelay);
+  }
+
+  /**
+   * detects change in state (user-inputted search text) and fires callback event
+   * @param nextProps
+   */
+  componentWillReceiveProps(nextProps: Props): void {
+    if (nextProps.searchText !== null && nextProps.searchText !== this.props.searchText) {
+      this.handleChange(nextProps.searchText, this.props.dataSource, {});
+    }
+    if (this.props.dataSource !== nextProps.dataSource)
+      this.setState({dataSourceRender: this.getDataSource(nextProps)});
+  }
+
+  getDataSource = (props: Props): Array<any> => {
+    if (props.exactMatch)
+      return props.dataSource.map(d => d.label);
+    return props.dataSource;
+  };
+
+  /**
+   * calls function to fire callback and updates searchText in state
+   * @param searchText
+   * @param dataSource
+   * @param params
+   */
+  handleChange = (searchText: string, dataSource: Array, params: Object) => {
+    if (this.props.exactMatch) {
+      // If we are looking for an exact match, then we want to update searchValue to pass
+      // back data to the server at that index from the dataSource
+      const filteredData = dataSource.filter(entry => entry.label === searchText);
+      if (filteredData.length > 0 && typeof this.props.setProps === 'function')
+        this.props.setProps({searchValue: filteredData[0].value});
+    }
+
+    // Always want to handle searchText updates
+    this.updateTextProps(searchText);
+    this.setState({searchText});
+  };
+
+  /**
+   * executes setProps function with searchText to update searchText in props;
+   * fires Dash callback event
+   * @param searchText
+   * @private
+   */
+  _updateTextProps = (searchText: string) => {
+    const { setProps } = this.props;
+
+    if (typeof setProps === 'function')
+      setProps({searchText});
+
+    if (typeof this.props.searchEndpointAPI !== 'undefined')
+      fetch(this.props.searchEndpointAPI, {
+        body: JSON.stringify({...this.props.searchJSONStructure, searchTerm: searchText}),
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, same-origin, *omit
+        headers: {
+          'content-type': 'application/json',
+        },
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'client', // *client, no-referrer
+      }).then(response => response.json()).then((response: {dataSource: Array<any>}) => {
+        this.setState({dataSourceRender: response.dataSource});
+      });
+
+    if (this.props.fireEvent) {
+      this.props.fireEvent({event: 'change'});
+    }
+  };
+
+  render() {
+    const { id, className, classes, filter, maxSearchResults, open, openOnFocus, style} = this.props;
+
+    return (
+      <div id={id} className={className}>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <MuiAutoComplete
+            classes={classes}
+            filterOptions={typeof this.props.searchEndpointAPI === 'undefined'
+                ? mapFilterToFunc[filter]
+                : () => true}
+            maxSearchResults={maxSearchResults}
+            onInputChange={(searchText: string, dataSource: Array, params: Object) =>
+              this.handleChange(searchText, dataSource, params)}
+            open={open}
+            openOnFocus={openOnFocus}
+            style={style}
+            inputValue={this.state.searchText}
+            options={this.state.dataSourceRender}
+            renderInput={(params) =>
+              <TextField {...params} label="With categories" variant="outlined" />}/>
+        </MuiThemeProvider>
+      </div>);
+  }
+}
+
+AutoComplete.defaultProps = defaultProps;
+
