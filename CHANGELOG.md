b9713e57ede87dd7f80cf1622034e8de5bec6265
Danny Kaminsky <dwkaminsky@gmail.com>
2020-10-13 12:29:31 -0500

    Add support for children to Snackbar

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## 4.0.2 2020-10-13
+### Adds
+- Adds support for children to Snackbar
+
 ## 4.0.1 2020-09-01
 ### Fixes
 - Fixes how the following components handle receiving props:
--- usage.py
+++ usage.py
@@ -111,8 +111,10 @@ app.layout = html.Div([
                     id='snackbar',
                     action='Update text',
                     actionStyles=dict(color='white'),
-                    message='You opened the Snackbar!')
+                    message='You opened the Snackbar!',
+                    autoHideDuration=10000000),
             ]),
+
 
             spacer,
 
